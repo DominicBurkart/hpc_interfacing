7d0f0b31947f9af7e4a90c4008629618227d032e
Dominic Burkart <1dominicb@gmail.com>
2018-02-08 09:13:53 -0500

    multiple cluster support
    added support for sending out batches to multiple clusters simultaneously using the load and launch commands (specify the cluster in the cluster_info function and set cluster="{your chosen cluster}" while calling load(); otherwise, the default_cluster will be used.)

--- sbatch.py
+++ sbatch.py
@@ -1,10 +1,8 @@
-
-
 '''For exporting individual python3 functions to a cluster for faster computation.
 
 Assumptions:
 
-1.) your cluster has the same modules as your local computer.
+1.) your cluster has the same modules as your local computer, and both are running the same version of python.
 
 2.) your cluster uses SLURM to manage jobs.
 
@@ -23,18 +21,10 @@ Usage:
 
 5.) drink a smoothie, if you want to!
 
-Limitations:
 
-1.) Commands can only be sent to one cluster at a time. All commands will be sent to run on the cluster specified in
-the last call to load(), or to this module's datafields (whichever one has been updated more recently).
-
-2.) If you're using a package supported by Conda / Anaconda, then you'll either need to have internet access on the
-cluster (this script will pull all of the packages in the script that sourced it and include them in the conda build if
-use_conda == True, use_anaconda == False, and _conda_package has a valid command to load conda; if
-use_anaconda == use_conda == True, it will try to open an anaconda environment with the name in _anaconda_env and will
-not do anything intelligent with import / package management beyond running your code in an anaconda environment. If you
-don't have use_conda set to true, this script won't look for it and will assume that your cluster has all of the
-packages necessary to run your scripts.
+FYI:
+You'll need to set up your cluster conda environment (or find an alternative) with any relevant packages if you
+aren't able to use the packages built into anaconda. Syntactic differences between python versions aren't accounted for.
 
 Dominic Burkart / dominicburkart.com
 '''
@@ -44,63 +34,77 @@ import os
 import pickle
 import types
 from time import time, sleep
+from functools import lru_cache
 
 #for managing imports from the source file
 import __main__
-from __main__ import * #for imports()
-# all libraries imported to sbatch will also need to be available on the cluster; use only standard libs
-import os
-import pickle
-import types
-from time import time, sleep
 
-# for managing imports from the source file
-import __main__
-from __main__ import *  # for imports()
+# ~~~~~~~~~~~~~~~~~~ beginning of the code that a user is expected to edit
 
 #set to false if all necessary packages are loaded into the standard python3 environment of the cluster.
-use_conda = False
-use_anaconda = False #only used if use_conda == True
+use_anaconda = False
+
+default_cluster = "cluster"
 
 # Note: use the cluster's filepath formatting for locations on the cluster, and the local formatting for local ones.
-_sbatch_file_directory = "local/path/on/your/machine/autopy" # directory of things to be sent to the cluster
-_archive_directory = "/local/path/autopy_archive" # for things that have been sent to the cluster
-_ssh_username = "cluster_username" # cluster username
-_ssh_key = "/path/to_shh_key" # cluster ssh key
-_host = "della5.princeton.edu" # cluster hostname
-_workdir = "/tigress/cluster_username/" # where on the cluster we should make a copy of _sbatch_file_directory
-_path_to_python3 = "intel-python/3.5/20150803_185146" # python3 path on the cluster (for "module load _path_to_python3")
-_mail = "email@princeton.edu" # email for SLURM messages
-_mail_type = "end,fail" # when to email
-_conda_package = "module load anaconda3"  # any package loading command that allows you to use conda (only used if conda == True)
-_anaconda_env = "autopy_anaconda" #name of the anaconda environment to run the code in if use_anaconda == True
+
+@lru_cache(maxsize=4)
+def cluster_info(c = default_cluster):
+    if c == "cluster1":
+        _sbatch_file_directory = "local path – unique to cluster"  # directory of things to be sent to the cluster
+        _archive_directory = "local path"  # for things that have been sent to the cluster
+        _ssh_username = "cluster"  # cluster username
+        _ssh_key = "key on local machine"  # cluster ssh key
+        _host = "della5.princeton.edu"  # cluster hostname
+        _workdir = "/tigress/username/"  # where on the cluster we should make a copy of _sbatch_file_directory.
+        _path_to_python3 = "intel-python/3.5/20150803_185146"  # python3 path on the cluster (for "module load _path_to...")
+        # leave _path_to_python3 blank if the version of python3 you want is loaded by default on the cluster.
+        _mail = "email@website.edu"  # email for SLURM messages
+        _mail_type = "end,fail"  # when to email
+        _conda_package = "module load anaconda3"  # any package loading command that allows you to use conda (only used if use_anaconda == True)
+        _anaconda_env = "autopy_anaconda"  # name of the anaconda environment to run the code in if use_anaconda == True
+    elif c == "cluster2":
+        _sbatch_file_directory = "local path – unique to cluster"
+        _archive_directory = "local path"
+        _ssh_username = "cluster"
+        _ssh_key = "key on local machine"
+        _host = "della5.princeton.edu"
+        _workdir = "/tigress/username/"
+        _path_to_python3 = "intel-python/3.5/20150803_185146"
+        _mail = "email@website.edu"
+        _mail_type = "end,fail"
+        _conda_package = "module load anaconda3"
+        _anaconda_env = "autopy_anaconda"
+    #define your own cluster info here using the same format as above!
+# ~~~~~~~~~~~~~~~~~~ end of the code that a user is expected to edit.
+    else:
+        raise UnknownClusterException("The following unrecognized value was passed as the cluster name: " +
+                                      str(c)+"\ntype: "+str(type(c)))
+    return locals()
+
 _active_batches = []  # will be populated by load() and emptied by launch()
+_active_clusters = [] # will also be populated by load() and emptied by launch()
 
-def imports():
-    '''
-    returns the names of all non-local imported libraries. May include repeated values.
-    '''
-    # from https://web.archive.org/web/20180118172759/https://stackoverflow.com/questions/4858100/how-to-list-imported-modules
-    for name, val in globals().items():
-        if isinstance(val, types.ModuleType):
-            yield val.__name__.split(".")[0]
-
-
-def str_set(inp):
-    ''' casts input into a set and then returns a string of all values in the input, split by one space between each value.
-    :return: string'''
-    s = set(inp)
-    o = ""
-    for x in s:
-        o = o + x + " "
-    return o
+class UnknownClusterException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 
 def launch():
     '''connects to the cluster and starts our sbatch jobs.'''
     import shutil, subprocess, pipes
 
-    def send(to, local_folder=_sbatch_file_directory, k=_ssh_key):
+    def send(to, local_folder, k):
+        '''
+        Uploads a folder to a given non-local address.
+
+        :param to: username@hostname:/path/on/remote/system
+        :param local_folder: local folder to send
+        :param k: SSH key file
+        :return: nothing
+        '''
         e = "\nsee http://geoweb.princeton.edu/it/sshkey.html for instructions of SSH keygen."
         if local_folder.endswith("/"):
             local_folder = local_folder[:len(local_folder) - 1]
@@ -119,56 +123,66 @@ def launch():
                 return
             raise Exception("SCP Error. Return code: " + str(c.returncode))
 
-    assert len(_active_batches) > 0  # don't contact the cluster if we have no reason to do so.
+    for c in _active_clusters:
+        clud = cluster_info(c=c)
 
-    if len(_active_batches) > 1:
-        print("preparing to launch " + str(len(_active_batches)) + " sbatch jobs.")
-    else:
-        print("preparing to launch one sbatch job.")
+        rel_jobs = [j[0] for j in _active_batches if j[1] == c]
 
-    # copy __main__ into the local folder
-    shutil.copy(__main__.__file__, _sbatch_file_directory)
-    print("main copied to the local directory.")
+        assert len(rel_jobs) > 0  # don't contact the cluster if we have no reason to do so.
 
-    # send necessary files to cluster
-    send(_ssh_username + "@" + _host + ":" + _workdir)
+        if len(rel_jobs) > 1:
+            print("preparing to launch " + str(len(rel_jobs)) + " sbatch jobs " + "to "+c)
+        else:
+            print("preparing to launch one sbatch job.")
 
-    # ssh into the cluster and sbatch the programs
-    cmdl = [['cd', _workdir + "/" + _sbatch_file_directory.split("/")[-1]]]
-    cmdl.extend([['sbatch', ac] for ac in _active_batches])
-    cmdl.append(['exit'])
-    cmd = ""
-    for l in cmdl:
-        cmd = cmd + ' '.join(pipes.quote(com) for com in l) + "\n"
-    c = subprocess.run(['ssh', '-i', _ssh_key, _ssh_username + '@' + _host, cmd])
-    if c.returncode != 0:
-        raise Exception("sbatch unsuccessful. Returncode: " + str(c.returncode))
-    else:
-        print("sbatch successful.")
+        # copy __main__ into the local folder
+        shutil.copy(__main__.__file__, clud["_sbatch_file_directory"])
+        print("main copied to the local directory.")
 
-    # archive the files we just sent, since they'll delete themselves on the cluster after running
-    sub = os.path.join(_archive_directory, "launched" + str(time()).replace(".", "_"))
-    os.mkdir(sub)
-    for f in os.listdir(_sbatch_file_directory):
-        if os.path.isfile(os.path.join(_sbatch_file_directory, f)):
-            shutil.move(os.path.join(_sbatch_file_directory, f), sub)
+        # send necessary files to cluster
+        send(clud["_ssh_username"] + "@" + clud["_host"] + ":" + clud["_workdir"], clud["_sbatch_file_directory"],
+             clud["_ssh_key"])
+
+        # ssh into the cluster and sbatch the programs
+        cmdl = [['cd', clud["_workdir"] + "/" + clud["_sbatch_file_directory"].split("/")[-1]]]
+        cmdl.extend([['sbatch', ac] for ac in rel_jobs])
+        cmdl.append(['exit'])
+        cmd = ""
+        for l in cmdl:
+            cmd = cmd + ' '.join(pipes.quote(com) for com in l) + "\n"
+        c = subprocess.run(['ssh', '-i', clud["_ssh_key"], clud["_ssh_username"] + '@' + clud["_host"], cmd])
+        if c.returncode != 0:
+            raise Exception("sbatch unsuccessful. Return code: " + str(c.returncode))
+        else:
+            print("sbatch successful.")
+
+        # archive the files we just sent, since they'll delete themselves on the cluster after running
+        sub = os.path.join(clud["_archive_directory"], "launched" + str(time()).replace(".", "_"))
+        os.mkdir(sub)
+        for f in os.listdir(clud["_sbatch_file_directory"]):
+            if os.path.isfile(os.path.join(clud["_sbatch_file_directory"], f)):
+                shutil.move(os.path.join(clud["_sbatch_file_directory"], f), sub)
 
     _active_batches.clear()  # we did it kids! The rest is up to the cluster.
+    _active_clusters.clear()
 
 
-def load(function_name, list_of_params=[], ssh_username=_ssh_username,
-         ssh_key=_ssh_key, maxtime="72:00:00",
-         nodes=1, cores=20, host=_host, workdir=_workdir,
+def load(function_name, list_of_params=[], cluster = default_cluster, maxtime="72:00:00",
+         nodes=1, cores=20, workdir=None,
          name="batch" + str(time()).replace(".", "_"),
-         path_to_python3=_path_to_python3,
-         mail=_mail, mail_type=_mail_type,
-         local=_sbatch_file_directory):
+         mail=None, mail_type=None,
+         local=None):
     '''Generates necessary helper scripts to run any individual function available in the program
     that calls this function on a cluster with python3 and SLURM.
 
     TODO: allow a bool to be passed on whether the whole directory should be sent to the cluster instead of just
     the file that called sbatch.
     '''
+
+    workdir = cluster_info(cluster)["_workdir"] if workdir is None else workdir
+    local = cluster_info(cluster)["_sbatch_file_directory"] if local is None else local
+    mail = cluster_info(cluster)["_mail"] if mail is None else mail
+    mail_type = cluster_info(cluster)["_mail_type"] if mail_type is None else mail_type
 
     # filenames for everything generated by this function
     tf_names = ["sbatch_tmp" + str(time()).replace(".", "_"), "wrapper" + str(time()).replace(".", "_") + ".py"]
@@ -190,7 +204,7 @@ def load(function_name, list_of_params=[], ssh_username=_ssh_username,
     pars = ["-N " + str(nodes),
             "-c " + str(cores),
             "-t " + str(maxtime),
-            "--workdir=" + os.path.join(workdir, _sbatch_file_directory.split("/")[-1]),
+            "--workdir=" + os.path.join(workdir, cluster_info(cluster)["_sbatch_file_directory"].split("/")[-1]),
             "--mail-user=" + mail,
             "--mail-type=" + mail_type]
     bash_script = "#!/bin/bash"
@@ -198,29 +212,18 @@ def load(function_name, list_of_params=[], ssh_username=_ssh_username,
         bash_script = bash_script + sb + p
 
     # load python3
-    bash_script = bash_script + "\n\nmodule load " + path_to_python3
+    if cluster_info(cluster)["_path_to_python3"] != "" and cluster_info(cluster)["_path_to_python3"] != None:
+        bash_script = bash_script + "\n\nmodule load " + cluster_info(cluster)["_path_to_python3"]
 
-    # load conda (if relevant)
-    if use_conda:
-        env_name = "autopy" + str(time()).replace(".", "_") if not use_anaconda else _anaconda_env
-        bash_script = bash_script + "\n" + _conda_package
-        if use_anaconda:
-            print("Assuming that the environment " + _anaconda_env +
-                  " has been preconfigured in the working directory of the cluster.")
-        else:
-            bash_script = bash_script + "\nconda create --name " + env_name + " -y python=3 " + str_set(imports())
-        bash_script = bash_script + "\nsource activate " + env_name
+    if use_anaconda:
+        bash_script = bash_script + "\n" + cluster_info(cluster)["_conda_package"] + "\nsource activate " + cluster_info(cluster)["_anaconda_env"]
 
     # add content (python)
     bash_script = bash_script + "\npython3 " + tf_names[1]
 
     for rm in ["\nrm " + n for n in tf_names]:
         bash_script = bash_script + rm
-        # ^ will remove unncessary run files on the cluster after the function is called (local files are kept).
-
-    # delete conda environment after use (unless anaconda was used)
-    if use_conda and not use_anaconda:
-        bash_script = bash_script + "\nconda remove --name " + env_name + " --all"
+        # ^ will remove unnecessary run files on the cluster after the function is called (local files are kept).
 
     f = open(os.path.join(local, tf_names[0]), "w")
     f.write(bash_script)
@@ -256,4 +259,7 @@ def l(file_path):
     f.write(pwrap)
     f.close()
 
-    _active_batches.append(tf_names[0])  # ready to be sent to the cluster + run by launch()!
+    _active_batches.append((tf_names[0], cluster))  # ready to be sent to the cluster + run by launch()!
+    if cluster not in _active_clusters:
+        _active_clusters.append(cluster)
+
